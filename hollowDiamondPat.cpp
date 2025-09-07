8f6fe0c86b68e909db6048e204d2adb3c629b0e2
ujjawal06-star <ujjwal9197us@gmail.com>
2025-09-07 11:18:38 +0530

    Hollow Diamond Pattern in cpp

---
+++ hollowDiamondPat.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0; j<n-i-1;j++){
+            cout<<" ";
+        }
+        cout<<"*";
+        if(i!=0){
+            for(int j = 0; j<2*i-1;j++){
+                cout<<" ";
+            }
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j = 0;j<=i;j++){
+            cout<<" ";
+        }
+        cout<<"*";
+        if(i!=n-2){
+            for(int j=0;j<2*(n-i)-5;j++){
+                cout<<" ";
+            }
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
